88cf8cd274c15326cdc19f759331812c2f4e53cb
Aranthar <dvkolomiets@gmail.com>
2024-04-22 00:12:03 +0500

    Create navigation to Statistics screen

--- app/src/main/java/com/example/aroundcompose/ui/screens/statistics/StatisticsScreen.kt
+++ app/src/main/java/com/example/aroundcompose/ui/screens/statistics/StatisticsScreen.kt
@@ -1,4 +1,41 @@
 package com.example.aroundcompose.ui.screens.statistics
 
-class StatisticsScreen {
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.aroundcompose.R
+import com.example.aroundcompose.ui.common.views.CustomTopAppBar
+
+class StatisticsScreen(
+    private val viewModel: StatisticsViewModel,
+    private val onBackClicked: () -> Unit,
+) {
+
+    @Composable
+    fun Create() {
+        val viewState by viewModel.getViewState().collectAsStateWithLifecycle()
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(start = 30.dp, top = 30.dp, end = 30.dp)
+        ) {
+            CustomTopAppBar(
+                textId = R.string.statistic,
+                onBackClick = onBackClicked
+            )
+        }
+    }
+
+    @Composable
+    private fun TeamsStatistics() {
+
+    }
 }
--- app/src/main/java/com/example/aroundcompose/ui/screens/statistics/StatisticsViewModel.kt
+++ app/src/main/java/com/example/aroundcompose/ui/screens/statistics/StatisticsViewModel.kt
@@ -1,4 +1,11 @@
 package com.example.aroundcompose.ui.screens.statistics
 
-class StatisticsViewModel {
+import com.example.aroundcompose.ui.common.models.BaseViewModel
+import com.example.aroundcompose.ui.screens.statistics.models.StatisticsEvent
+import com.example.aroundcompose.ui.screens.statistics.models.StatisticsViewState
+import javax.inject.Inject
+
+class StatisticsViewModel @Inject constructor() :
+    BaseViewModel<StatisticsViewState, StatisticsEvent>(initialState = StatisticsViewState()) {
+
 }
