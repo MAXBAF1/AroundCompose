68359bdcc135df55a14ddbb27a858fdcc48b1ce1
Aranthar <dvkolomiets@gmail.com>
2024-05-05 11:10:28 +0500

    Add logic for friends screen

--- app/src/main/java/com/example/aroundcompose/ui/screens/friends/FriendsScreen.kt
+++ app/src/main/java/com/example/aroundcompose/ui/screens/friends/FriendsScreen.kt
@@ -1,16 +1,15 @@
 package com.example.aroundcompose.ui.screens.friends
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -18,13 +17,11 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.aroundcompose.R
-import com.example.aroundcompose.ui.common.enums.Teams
 import com.example.aroundcompose.ui.common.views.CustomTopAppBar
 import com.example.aroundcompose.ui.common.views.SearchView
 import com.example.aroundcompose.ui.screens.friends.models.FriendData
 import com.example.aroundcompose.ui.screens.friends.models.FriendsEvent
 import com.example.aroundcompose.ui.screens.friends.views.FriendCard
-import java.util.UUID
 
 class FriendsScreen(
     private val viewModel: FriendsViewModel,
@@ -34,8 +31,6 @@ class FriendsScreen(
     @Composable
     fun Create() {
         val viewState by viewModel.getViewState().collectAsStateWithLifecycle()
-
-        var isEventSheetShowed by remember { mutableStateOf(false) }
 
         Box {
             Image(
@@ -67,49 +62,34 @@ class FriendsScreen(
                     }
                 )
 
-                FriendCard(
-                    friendData = FriendData(
-                        id = UUID.randomUUID(),
-                        level = 3,
-                        team = Teams.YELLOW,
-                        position = 1,
-                        imageId = R.drawable.avatar_example,
-                        name = "MagicHero11",
-                        score = 560
-                    ),
-                    onMoreInfoClick = onMoreInfoClick
-                ).Create(Modifier)
-
-                FriendCard(
-                    friendData = FriendData(
-                        id = UUID.randomUUID(),
-                        level = 5,
-                        team = Teams.LIGHT_BLUE,
-                        position = 2,
-                        imageId = R.drawable.avatar_example,
-                        name = "IdiRabotaiOlen'",
-                        score = 320
-                    ),
-                    onMoreInfoClick = onMoreInfoClick
-                ).Create(Modifier.padding(top = 16.dp))
-
-                FriendsContainer(
-                    friendsList = viewState.friendsList,
-                    onMoreInfoClick = onMoreInfoClick
-                )
+                if (viewState.searchText == "") {
+                    FriendsContainer(
+                        list = viewState.friendsList,
+                        onMoreInfoClick = onMoreInfoClick
+                    )
+                } else {
+                    FriendsContainer(
+                        list = viewState.friendsFilteredList,
+                        onMoreInfoClick = onMoreInfoClick
+                    )
+                }
             }
         }
     }
 
     @Composable
     private fun FriendsContainer(
-        friendsList: List<FriendData>,
+        list: List<FriendData>,
         onMoreInfoClick: () -> Unit,
     ) {
-        LazyColumn {
-            items(friendsList.size) { index ->
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ) {
+            items(list.size) { index ->
                 FriendCard(
-                    friendData = friendsList[index],
+                    friendData = list[index],
                     onMoreInfoClick = onMoreInfoClick
                 ).Create(
                     modifier = if (index != 0) {
--- app/src/main/java/com/example/aroundcompose/ui/screens/friends/FriendsViewModel.kt
+++ app/src/main/java/com/example/aroundcompose/ui/screens/friends/FriendsViewModel.kt
@@ -1,15 +1,92 @@
 package com.example.aroundcompose.ui.screens.friends
 
+import com.example.aroundcompose.R
+import com.example.aroundcompose.ui.common.enums.Teams
 import com.example.aroundcompose.ui.common.models.BaseViewModel
 import com.example.aroundcompose.ui.screens.friends.models.FriendData
 import com.example.aroundcompose.ui.screens.friends.models.FriendsEvent
 import com.example.aroundcompose.ui.screens.friends.models.FriendsViewState
 import kotlinx.coroutines.flow.update
+import java.util.UUID
 import javax.inject.Inject
 
 class FriendsViewModel @Inject constructor() :
     BaseViewModel<FriendsViewState, FriendsEvent>(initialState = FriendsViewState()) {
-    private val friendsList: MutableList<FriendData> = mutableListOf()
+    private val friendsList: MutableList<FriendData> = mutableListOf(
+        FriendData(
+            id = UUID.randomUUID(),
+            level = 3,
+            team = Teams.YELLOW,
+            position = 1,
+            imageId = R.drawable.avatar_example,
+            name = "MagicHero11",
+            score = 560
+        ),
+        FriendData(
+            id = UUID.randomUUID(),
+            level = 5,
+            team = Teams.LIGHT_BLUE,
+            position = 2,
+            imageId = R.drawable.avatar_example,
+            name = "IdiRabotaiOlen'",
+            score = 320
+        ),
+        FriendData(
+            id = UUID.randomUUID(),
+            level = 8,
+            team = Teams.BLUE,
+            position = 3,
+            imageId = R.drawable.avatar_example,
+            name = "Test228",
+            score = 220
+        ),
+        FriendData(
+            id = UUID.randomUUID(),
+            level = 1,
+            team = Teams.PURPLE,
+            position = 4,
+            imageId = R.drawable.avatar_example,
+            name = "Dinozavr",
+            score = 180
+        ),
+        FriendData(
+            id = UUID.randomUUID(),
+            level = 2,
+            team = Teams.YELLOW,
+            position = 5,
+            imageId = R.drawable.avatar_example,
+            name = "Fredy Fasber",
+            score = 150
+        ),
+        FriendData(
+            id = UUID.randomUUID(),
+            level = 8,
+            team = Teams.LIGHT_BLUE,
+            position = 6,
+            imageId = R.drawable.avatar_example,
+            name = "Oleg Mongol",
+            score = 100
+        ),
+        FriendData(
+            id = UUID.randomUUID(),
+            level = 1,
+            team = Teams.PURPLE,
+            position = 7,
+            imageId = R.drawable.avatar_example,
+            name = "Outsider",
+            score = 80
+        ),
+        FriendData(
+            id = UUID.randomUUID(),
+            level = 2,
+            team = Teams.BLUE,
+            position = 8,
+            imageId = R.drawable.avatar_example,
+            name = "Anime",
+            score = 50
+        ),
+    )
+    private val friendsFilteredList: MutableList<FriendData> = mutableListOf()
     private var searchText: String = ""
     private var isEventSheetShowed: Boolean = false
 
@@ -17,12 +94,24 @@ class FriendsViewModel @Inject constructor() :
         when (viewEvent) {
             is FriendsEvent.OnSearchTextChanged -> {
                 searchText = viewEvent.text
+                searchList(searchText)
 
                 viewState.update {
                     it.copy(
-                        searchText = searchText
+                        searchText = searchText,
+                        friendsFilteredList = friendsFilteredList
                     )
                 }
+            }
+        }
+    }
+
+    private fun searchList(text: String) {
+        friendsFilteredList.clear()
+
+        friendsList.forEach { friendCard ->
+            if (friendCard.name.lowercase().contains(text.lowercase())) {
+                friendsFilteredList.add(friendCard)
             }
         }
     }
