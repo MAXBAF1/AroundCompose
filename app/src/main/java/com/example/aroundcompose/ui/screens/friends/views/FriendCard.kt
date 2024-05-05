d8c000f87fec7a807a1584b4459ee367ca2455bc
Aranthar <dvkolomiets@gmail.com>
2024-05-05 14:42:26 +0500

    Fix friend card click

--- app/src/main/java/com/example/aroundcompose/ui/screens/friends/views/FriendCard.kt
+++ app/src/main/java/com/example/aroundcompose/ui/screens/friends/views/FriendCard.kt
@@ -1,8 +1,6 @@
 package com.example.aroundcompose.ui.screens.friends.views
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
@@ -25,27 +23,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.graphics.shapes.CornerRounding
-import androidx.graphics.shapes.RoundedPolygon
 import com.example.aroundcompose.R
 import com.example.aroundcompose.ui.common.views.HexagonImage
 import com.example.aroundcompose.ui.common.views.LevelView
 import com.example.aroundcompose.ui.screens.friends.models.FriendData
 import com.example.aroundcompose.ui.theme.JetAroundTheme
-import com.example.aroundcompose.utils.RoundedPolygonShape
 
 
 class FriendCard(
     private val friendData: FriendData,
     private val onMoreInfoClick: () -> Unit,
 ) {
-// Кликалось на всю
     @Composable
     fun Create(modifier: Modifier) {
-        val shape = RoundedPolygonShape(
-            RoundedPolygon(numVertices = 6, rounding = CornerRounding(radius = 0.14F))
-        )
-
         Card(
             shape = JetAroundTheme.shapes.friendShape,
             elevation = CardDefaults.cardElevation(JetAroundTheme.shadows.mapElementsShadow),
@@ -55,56 +45,55 @@ class FriendCard(
                 disabledContainerColor = JetAroundTheme.colors.primaryBackground,
                 disabledContentColor = JetAroundTheme.colors.titleColor
             ),
-            modifier = modifier
-                .fillMaxWidth()
+            modifier = modifier.fillMaxWidth()
         ) {
             Row(
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.SpaceBetween,
                 modifier = Modifier
                     .fillMaxWidth()
+                    .clip(shape = JetAroundTheme.shapes.friendShape)
+                    .clickable(
+                        onClick = onMoreInfoClick,
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = rememberRipple(color = JetAroundTheme.colors.textColor)
+                    )
                     .padding(vertical = 8.dp, horizontal = 18.dp)
             ) {
                 Row(
                     verticalAlignment = Alignment.CenterVertically,
                     horizontalArrangement = Arrangement.Start,
-                ) {
-                    Text(
-                        text = "${friendData.position}.",
-                        style = JetAroundTheme.typography.semiBold,
-                        color = JetAroundTheme.colors.titleColor
-                    )
-                    HexagonImage(
-                        imageId = friendData.imageId,
-                        team = friendData.team,
-                        border = 2.dp,
-                        modifier = Modifier
-                            .padding(start = 8.dp)
-                            .size(50.dp)
-                    )
-
-                    TextInfo(
-                        name = friendData.name,
-                        level = friendData.level,
-                        score = friendData.score
-                    )
-                }
-
+                    content = { ContainerInfo() }
+                )
                 Icon(
                     painter = painterResource(id = R.drawable.ic_more_info),
                     contentDescription = "more info",
-                    tint = JetAroundTheme.colors.lightTint,
-                    modifier = Modifier
-                        .clip(shape = CircleShape)
-                        .clickable(
-                            onClick = onMoreInfoClick,
-                            interactionSource = remember { MutableInteractionSource() },
-                            indication = rememberRipple(color = JetAroundTheme.colors.textColor)
-                        )
-                        .padding(vertical = 4.dp, horizontal = 7.dp)
+                    tint = JetAroundTheme.colors.lightTint
                 )
             }
         }
+    }
+
+    @Composable
+    private fun ContainerInfo() {
+        Text(
+            text = "${friendData.position}.",
+            style = JetAroundTheme.typography.semiBold,
+            color = JetAroundTheme.colors.titleColor
+        )
+        HexagonImage(
+            imageId = friendData.imageId,
+            team = friendData.team,
+            border = 2.dp,
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .size(50.dp)
+        )
+        TextInfo(
+            name = friendData.name,
+            level = friendData.level,
+            score = friendData.score
+        )
     }
 
     @Composable
@@ -124,7 +113,6 @@ class FriendCard(
                     style = JetAroundTheme.typography.mediumBold,
                     color = color
                 )
-
                 LevelView(
                     level = level,
                     textColor = JetAroundTheme.colors.titleColor,
@@ -136,7 +124,6 @@ class FriendCard(
                         .padding(top = 4.dp) // Inner padding
                 )
             }
-
             Text(
                 text = "захвачено: $score клеток",
                 style = JetAroundTheme.typography.textDesc,
