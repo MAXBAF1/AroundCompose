d3e3528c92c7b8552c711239dcc7809a181cfef7
Aranthar <dvkolomiets@gmail.com>
2024-05-25 13:50:59 +0500

    New skills data

--- app/src/main/java/com/example/aroundcompose/ui/screens/skills/models/SkillsViewState.kt
+++ app/src/main/java/com/example/aroundcompose/ui/screens/skills/models/SkillsViewState.kt
@@ -1,36 +1,5 @@
 package com.example.aroundcompose.ui.screens.skills.models
 
-import com.example.aroundcompose.R
-
 data class SkillsViewState(
-    val mapOfSkills: Map<SkillType, SkillData> = mapOf(
-        SkillType.SQUARE to SkillData(
-            iconId = R.drawable.ic_square,
-            imageId = R.drawable.skill_square,
-            titleId = R.string.title_square,
-            descriptionId = R.string.desc_square,
-            currentLevel = 2
-        ),
-        SkillType.BOMB to SkillData(
-            iconId = R.drawable.ic_bomb,
-            imageId = R.drawable.skill_bomb,
-            titleId = R.string.title_bomb,
-            descriptionId = R.string.desc_bomb,
-            currentLevel = 10
-        ),
-        SkillType.INVISIBLE to SkillData(
-            iconId = R.drawable.ic_invisible_user,
-            imageId = R.drawable.skill_invisible,
-            titleId = R.string.title_invisible,
-            descriptionId = R.string.desc_invisible,
-            currentLevel = 5
-        ),
-        SkillType.MINE to SkillData(
-            iconId = R.drawable.ic_mine,
-            imageId = R.drawable.skill_mine,
-            titleId = R.string.title_mine,
-            descriptionId = R.string.desc_mine,
-            currentLevel = 8
-        )
-    ),
+    val skills: Skills = Skills(),
 )
